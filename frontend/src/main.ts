2daa2a8914657b475ac38e29a08fb51024a0a394
Guilherme Sousa <108591854+Guisousa5@users.noreply.github.com>
2025-12-01 17:49:21 -0300

    Import HttpClientModule for HTTP requests

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,13 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http'; // <-- IMPORTAR AQUI
+import { HttpClientModule } from '@angular/common/http'; 
 
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(
       BrowserAnimationsModule,
-      HttpClientModule // <-- Adicionar HttpClientModule aqui
+      HttpClientModule 
     ),
     provideRouter(routes),
   ]
